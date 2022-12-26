2f7daa5ed16ec3a86a3e734d39dfbdba0e1ddb83
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2022-12-25 19:20:39 -0500

    add get routes and ejs files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//dependencies
 const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -5,5 +6,28 @@ const methodOverride = require('method-override')
 const app = express()
 const PORT = process.env.PORT
 
+//middleware
 
+//ROUTES
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/signin', (req, res) => {
+    res.render('signin.ejs')
+})
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs')
+})
+app.get('/:id', (req, res) => {
+    res.render('allchats.ejs')
+})
+app.get('/:id/:user', (req, res) => {
+    res.render('chats.ejs')
+})
+
+
+
+
+
+//listener
 app.listen(process.env.PORT || 3000, () => {console.log('listening...')})
