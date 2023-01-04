c23ae9a696950bda8d7b8676042ca2a885ecbd5f
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-01-04 18:46:40 -0500

    chain routes with app.route()

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const User = require("./models/user")
 
 //middleware
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 //database configuration
@@ -25,34 +26,46 @@ DB.on("disconnected", () => console.log("mongo is disconnected..."))
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
-app.get("/signin", (req, res) => {
-    res.render("signin.ejs")
-})
-app.get("/signup", (req, res) => {
-    res.render("signup.ejs")
-})
-app.post("/signup", (req, res) => {
-    console.log(req.body)
-    User.create(req.body, () => {
-        res.redirect("/signup")
+app.route('/signin')
+    .get((req, res) => {
+        res.render("signin.ejs")
     })
-})
-app.post("/signin", (req, res) => {
-    User.find(
-        { username: req.body.username, password: req.body.password },
-        (err, foundUser) => {
-            res.redirect(`/${foundUser[0].username}`)
-        }
-    )
-})
-app.get("/:username", (req, res) => {
-    User.find({username: req.params.username,},
-        (err, foundUser) => 
-        {
-            console.log('the found user is ', foundUser[0].username)
-            res.render("allchats.ejs", { user: foundUser[0] })
-        }
-    )
+    .post((req, res) => {
+        User.find(
+            {
+                username: req.body.username,
+                password: req.body.password, 
+            },
+            (err, foundUser) => {
+                console.log(foundUser)
+                res.redirect(`/${foundUser[0].username}`)
+            }
+        )
+    })
+app.route('/signup')
+    .get((req, res) => {
+        res.render("signup.ejs")
+    })
+    .post((req, res) => {
+        console.log(req.body)
+        User.create(req.body, () => {
+            res.redirect(`/${req.body.username}`)
+        })
+    })
+
+app.route('/:username')
+    .get((req, res) => {
+        res.render('allchats.ejs')
+        // User.find({username: req.params.username},
+        //     (err, foundUser) => {
+                
+        //         res.render("allchats.ejs", { user: foundUser[0].username })
+                
+        //     }
+        // )
+    })
+    .delete((req, res) => {
+        User.deleteOne({username: req.params.username})
 })
 app.get("/:username/:chattingwith", (req, res) => {
     res.render("chat.ejs")
