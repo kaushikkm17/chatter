ea75ebfc8519fea95acefd6b53fd330a0f3fafd9
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-01-05 08:32:49 -0500

    wip: need to fix chats page view

--- models/user.js
+++ models/user.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
-
 const userSchema = new Schema({
     username: String,
     email: { type: String, lowercase: true, required: true, unique: true },
     password: { type: String, required: true, minLength: 8 },
-    textsreceived: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "TextReceived",
-      },
-      { timestamps: true },
-    ],
     textssent: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "TextSent",
-      },
-      { timestamps: true },
-    ]
-});
+        {
+          to: { type: String, required: true },
+          message: { type: String, required: true },
+          createdAt: { type: Date, default: () => Date.now() },
+        },
+    ],
+    textsreceived: [
+        {
+          from: { type: String, required: true },
+          message: { type: String, required: true },
+          createdAt: { type: Date, default: () => Date.now() },
+        },
+    ],
 
+});
 module.exports = mongoose.model('User', userSchema)
-  
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //dependencies
-const express = require("express")
-require("dotenv").config()
-const mongoose = require("mongoose")
-const methodOverride = require("method-override")
+const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const app = express()
 const PORT = process.env.PORT
-const User = require("./models/user")
+const User = require('./models/user.js')
+
+
 
 //middleware
 app.use(express.urlencoded({ extended: true }))
@@ -18,23 +20,23 @@ mongoose.connect(process.env.DATABASE_URI, {
     useUnifiedTopology: true,
 })
 const DB = mongoose.connection
-DB.on("error", (err) => console.log(err))
-DB.on("connected", () => console.log("mongo is connected..."))
-DB.on("disconnected", () => console.log("mongo is disconnected..."))
+DB.on('error', (err) => console.log(err))
+DB.on('connected', () => console.log('mongo is connected...'))
+DB.on('disconnected', () => console.log('mongo is disconnected...'))
 
 //ROUTES index, new, delete, update, create, edit, show
-app.get("/", (req, res) => {
-    res.render("index.ejs")
+app.get('/', (req, res) => {
+    res.render('index.ejs')
 })
 app.route('/signin')
     .get((req, res) => {
-        res.render("signin.ejs")
+        res.render('signin.ejs')
     })
     .post((req, res) => {
         User.find(
             {
                 username: req.body.username,
-                password: req.body.password, 
+                password: req.body.password,
             },
             (err, foundUser) => {
                 console.log(foundUser)
@@ -44,26 +46,67 @@ app.route('/signin')
     })
 app.route('/signup')
     .get((req, res) => {
-        res.render("signup.ejs")
+        res.render('signup.ejs')
     })
     .post((req, res) => {
-        console.log(req.body)
-        User.create(req.body, () => {
+        console.log(User);
+        User.create({
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password,
+        }, () => {
             res.redirect(`/${req.body.username}`)
         })
     })
 
 app.route('/:username')
     .get((req, res) => {
-        res.render('allchats.ejs', {username: req.params.username})
+        const chatWith = req.params.username === 'username1' ? 'username1' : 'username2'
+        res.render('allchats.ejs', { username: req.params.username, chatWith})
     })
     .delete((req, res) => {
-        User.findOneAndRemove({username: req.params.username})
-            .then(res.redirect('/'))
+        User.findOneAndRemove({ username: req.params.username }).then(
+            res.redirect('/')
+        )
     })
-app.get("/:username/:chattingwith", (req, res) => {
-    res.render("chat.ejs")
-})
+app.route('/:username/:chattingwith')
+    .get((req, res) => {
+        // async function run () {
+        //     const user = await User.findOne({username:req.params.username})
+        //     const textsReceived = user.textsreceived
+        //     const textsSent = user.textssent
+        //     textsReceived.forEach(text => text.class = 'their-msg')
+        //     textsSent.forEach(text => text.class = 'your-msg')
+            
+
+        //         res.render('chat.ejs', {
+        //             username: req.params.username,
+        //             chattingWith: req.params.chattingwith,
+        //             user:user
+        //         })
+        // }
+        res.render('chat.ejs', {
+            username: req.params.username,
+            chattingWith: req.params.chattingwith,
+        })
+    })
+    .put((req, res) => {
+        User.findOneAndUpdate(
+            { username: req.params.chattingwith },
+            { $push: { textsreceived: { from: req.params.username, message: req.body.message, createdAt: new Date() } } },
+            { new: true },
+            )
+        .then(
+        User.findOneAndUpdate(
+            { username: req.params.username },
+            { $push: { textssent: { to: req.params.chattingwith, message: req.body.message, createdAt: new Date() } } },
+            { new: true },
+            )
+        )
+        .then(() => res.redirect(`/${req.params.username}/${req.params.chattingwith}`))
+        
+        
+    })
 
 //listener
-app.listen(PORT || 3000, () => console.log("listening..."))
+app.listen(PORT || 3000, () => console.log('listening...'))
